25f319bf808d9d849ae3f57e375aa71d12c0a771
ikkim01 <101634412+ikkim01@users.noreply.github.com>
2023-07-05 18:38:23 +0900

    Fix:pc버전 즐겨찾기 안닫히는 문제 수정

--- components/SlideModal.tsx
+++ components/SlideModal.tsx
@@ -75,7 +75,7 @@ const SlideModal = ({ mainRef }) => {
   return (
     <div
       ref={modalRef}
-      className={`bg-menuBackground/90 fixed mobile:w-full smallPc:w-full pc:w-[450px] mobile:bottom-[-450px] smallPc:bottom-[-450px] space-y-3 pb-5 transition-transform duration-500 z-[1] h-[300px] flex mobile:flex-col smallPc:flex-col items-center overflow-scroll pc:right-[-450px] pc:top-0 pc:pt-[93.5px] pc:h-full ${
+      className={`bg-menuBackground/90 fixed mobile:w-full smallPc:w-full pc:w-[450px] mobile:bottom-[-450px] smallPc:bottom-[-450px] space-y-3 pb-5 transition-transform duration-500 z-[1] h-[300px] flex mobile:flex-col smallPc:flex-col mobile:items-center smallPc:items-center overflow-scroll pc:right-[-450px] pc:top-0 pc:pt-[93.5px] pc:h-full ${
         menu === "favorite" || menu === "menu"
           ? "mobile:translate-y-[-610px] smallPc:translate-y-[-610px] pc:translate-x-[-450px]"
           : "mobile:translate-y-[0px] smallPc:translate-y-[0px] pc:translate-x-0"
@@ -87,7 +87,7 @@ const SlideModal = ({ mainRef }) => {
         className={
           menu === "none"
             ? "hidden"
-            : "mobile:w-full smallPc:w-full flex justify-center mobile:sticky smallPc:sticky pc:relative mobile:top-0 smallPc:top-0 z-[2] bg-menuBackground/90 pc:left-0"
+            : "mobile:w-full smallPc:w-full flex justify-center mobile:sticky smallPc:sticky pc:relative mobile:top-0 smallPc:top-0 z-[2] bg-menuBackground/90 pc:left-0 pc:items-center"
         }
       >
         <svg
--- components/Title.tsx
+++ components/Title.tsx
@@ -52,8 +52,8 @@ const Title = ({ title }: propsType) => {
 
   console.log(navMenu);
   return (
-    <header className="flex justify-center items-center h-[93.5px] border-b text-[35px] text-grey bg-fourthYellow w-full mobile:px-5 px-[5vw] relative z-[4] pc:fixed">
-      <button onClick={() => router.push("/")} className="absolute left-[5vw]">
+    <header className="flex justify-between items-center h-[93.5px] px-[5vw] border-b text-[35px] text-grey bg-fourthYellow w-full mobile:px-5 relative z-[4] pc:fixed">
+      <button onClick={() => router.push("/")}>
         <Image
           src="/img/homeIcon.svg"
           alt="goHome"
@@ -63,7 +63,7 @@ const Title = ({ title }: propsType) => {
         />
       </button>
       <h1 className="pc:hidden">{title}</h1>
-      <nav className="mobile:hidden smallPc:hidden h-full flex ">
+      <nav className="mobile:hidden smallPc:hidden h-full flex">
         <ul className="flex space-x-10 font-[300] h-full hover:text-gray-500">
           {MENU.map((menu) => {
             if (menu.key !== "HOME") {
@@ -200,26 +200,28 @@ const Title = ({ title }: propsType) => {
           priority={true}
         />
       </button> */}
-      <button
-        className="absolute right-[5vw] mobile:opacity-0 smallPc:hidden w-[35px]"
-        name="favorite"
-        onClick={handleMenu}
-      >
-        <svg
-          width="35px"
-          height="35px"
-          viewBox="0 0 24 24"
-          fill={menu === "favorite" ? "#30A2FF" : "none"}
-          xmlns="http://www.w3.org/2000/svg"
+      <div className="mobile:opacity-0 smallPc:opacity-0 w-[35px] flex items-center">
+        <button
+          className="mobile:hidden smallPc:hidden button"
+          name="favorite"
+          onClick={handleMenu}
         >
-          <path
-            d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
-            stroke={menu === "favorite" ? "#30A2FF" : "#041C32"}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
+          <svg
+            width="35px"
+            height="35px"
+            viewBox="0 0 24 24"
+            fill={menu === "favorite" ? "#30A2FF" : "none"}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.65 8.93274L12.4852 4.30901C12.2923 3.89699 11.7077 3.897 11.5148 4.30902L9.35002 8.93274L4.45559 9.68243C4.02435 9.74848 3.84827 10.2758 4.15292 10.5888L7.71225 14.2461L6.87774 19.3749C6.80571 19.8176 7.27445 20.1487 7.66601 19.9317L12 17.5299L16.334 19.9317C16.7256 20.1487 17.1943 19.8176 17.1223 19.3749L16.2878 14.2461L19.8471 10.5888C20.1517 10.2758 19.9756 9.74848 19.5444 9.68243L14.65 8.93274Z"
+              stroke={menu === "favorite" ? "#30A2FF" : "#041C32"}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
     </header>
   );
 };
